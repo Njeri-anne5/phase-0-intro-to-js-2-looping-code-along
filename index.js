b1e47f756a02609a227753bf546c8491398c61b2
+ <+>
2022-08-23 12:37:28 -0400

    Complete

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const names=["Guadalupe", "Ollie", "Aki"];
+const event="surprise";
 
-function writeCards(names){
+function writeCards(names,event){
+    let thankyouCards=[]
+
     for(let i=0;i<names.length;i++){
-        console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
-    
+            thankyouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+     }
+
+
+    return thankyouCards;
+
     }
 
-    return names;
-
-}
-writeCards(names);
-
-//countDown from 10
-let countDown=10;
-while(countDown>=0){
-    console.log(countDown--);
-    
-}
-//countDown from 4
-for (let countDown=4;countDown>=0;countDown--){
+//countDown from 10- for loop
+for (let countDown=11;countDown>=0;countDown--){
     console.log(countDown); 
 }
 
+
+//countDown from 4--while loop
+function countDown(){
+let bcountDown=11;
+while(bcountDown--){
+    console.log(bcountDown);
+}
+}
